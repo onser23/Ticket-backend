3101abc95e9644fc111ac1a90f8b0c32a567d1ac
onser23 <semed.semedzade@yahoo.com>
2026-06-27 16:21:41 +0400

    new

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const { connectDB } = require("./config/db");
 
 const app = express();
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//   credentials: true,
+// }));
+
 app.use(cors());
-app.use(express.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
