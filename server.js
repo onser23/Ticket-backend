ebb77df0759ccbebf502a09db6fc716f375a9229
onser23 <semed.semedzade@yahoo.com>
2026-06-27 16:19:33 +0400

    new

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const { connectDB } = require("./config/db");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "https://ticket.webaku.az",
-      "http://localhost:3000",
-      "https://ticket-backend-eight.vercel.app",
-    ],
-    credentials: true,
-  }),
-);
+app.use(cors());
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
