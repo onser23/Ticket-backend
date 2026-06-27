278de9e9f2d13742e2427768b2930eb7c7dbab39
onser23 <semed.semedzade@yahoo.com>
2026-06-27 15:47:18 +0400

    new

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const app = express();
 
 app.use(
   cors({
-    // origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: ["https://ticket.webaku.az", "http://localhost:3000"],
     credentials: true,
   }),
 );
-// app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
