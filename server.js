6c18f44a5dc91440756fe4f3f86ab4238e5c587f
onser23 <semed.semedzade@yahoo.com>
2026-06-27 16:25:52 +0400

    new

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 // }));
 
 app.use(cors());
+app.use(express.json({ limit: "10mb" }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -20,7 +21,7 @@ app.use(function (req, res, next) {
   );
   next();
 });
-app.use(express.json({ limit: "10mb" }));
+
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
 app.use(
