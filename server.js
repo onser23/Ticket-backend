2ed62c9a4b8d48a02fc4c6b6cc682df09638c380
onser23 <semed.semedzade@yahoo.com>
2026-06-28 12:22:33 +0400

    aa

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
 const cors = require("cors");
+const morgan = require("morgan");
 const { connectDB } = require("./config/db");
 
 const app = express();
